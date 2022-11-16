b3bf9fd790109e458870993a01330a7d6b884f1a
quintas <quintas.dev.ludovic@gamil.com>
2022-11-16 22:23:06 +0100

    titre et formulaire

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  //states (états,données)
+  const [prenom, setPrenom] = useState('');
+
+  //comportements
+  const handleChange = (e) => {
+    const afterChange = e.target.value;
+    setPrenom(afterChange);
+  };
+  const handleClick = () => {
+    alert('Bonjour' + '' + prenom);
+  };
+  // affichage (render)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <h2>Connectez-vous</h2>
+      <form action="">
+        <input
+          type="text"
+          value={prenom}
+          onChange={handleChange}
+          placeholder="Entrez votre prénom"
+        />
+        <button onClick={handleClick}> Accéder à votre espace</button>
+      </form>
     </div>
   );
 }
-
 export default App;
