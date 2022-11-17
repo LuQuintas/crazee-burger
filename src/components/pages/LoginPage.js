7974440285ebdeeb04bb32050f9f84ea5bcb99a7
quintas <quintas.dev.ludovic@gamil.com>
2022-11-17 15:09:11 +0100

    ✨ F01 : LOGIN> add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  // state(etats,données)
+  const [inputValue, setInputValue] = useState('Bob');
+
+  //comportements
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue('');
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  //affichage(render)
+  return (
+    <div>
+      <h1>Bienvenue à vous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={inputValue}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre nom..."
+          required
+        />
+        <button>Accedez àvotre espace</button>
+      </form>
+    </div>
+  );
 };
